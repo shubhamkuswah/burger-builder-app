693db581be801fe8168de3dde2fd1370fa26bde8
shubhamkuswah <kuswahshubham@gmail.com>
2021-02-15 17:12:53 +0530

    React lecture 156

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Logo from '../../Logo/Logo';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItem/NavigationItem';
