f88e0646312c3b9183c9d3e07922e9088c8033ef
shubhamkuswah <kuswahshubham@gmail.com>
2021-02-15 16:24:06 +0530

    React lecture 149

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 
-const layout =(props)=> (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer : true
+    }
 
-export default layout;
+    sideDrawerCloseHandler =()=>{
+        this.setState({showSideDrawer: false})
+    }
+
+    render() {
+        return(
+        <Aux>
+        <Toolbar/>
+        <SideDrawer open= {this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+        <main className={classes.Content}>
+            {this.props.children}
+        </main>
+        </Aux>
+    )
+    }
+}
+
+export default Layout;
